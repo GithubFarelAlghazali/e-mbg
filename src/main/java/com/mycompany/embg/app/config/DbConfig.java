d91f4c58b8c1a45693526a4c1db4c6d20e9d26e3
GithubFarelAlghazali <farelgithub@gmail.com>
2026-06-03 20:40:21 +0700

    feat: add basic db config

--- src/main/java/com/mycompany/embg/app/config/DbConfig.java
+++ src/main/java/com/mycompany/embg/app/config/DbConfig.java
@@ -9,5 +9,6 @@ package com.mycompany.embg.app.config;
  * @author User
  */
 public class DbConfig {
-    
+    public static final String SUPABASE_URL = "https://gpychukfwcmuwhryvsxl.supabase.co/rest/v1/";
+    public static final String SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdweWNodWtmd2NtdXdocnl2c3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMTQ0NDQsImV4cCI6MjA5NTg5MDQ0NH0.apri8POg1_6rGQ7RUVrQHNmklqXhrz9JlfSRaZhzK4w";
 }
