790bebeed8209d189b4567961b47cf0ef1d32680
GithubFarelAlghazali <farelgithub@gmail.com>
2026-06-03 09:10:16 +0700

    feat: add siswa-khusus and jadwal class model

---
+++ src/main/java/com/mycompany/embg/app/models/SiswaKebutuhanKhusus.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.embg.app.models;
+
+/**
+ *
+ * @author User
+ */
+public class SiswaKebutuhanKhusus {
+    private String alergi;
+    private String nama;
+    
+    public SiswaKebutuhanKhusus(String alergi, String nama){
+        this.alergi = alergi;
+        this.nama = nama;
+    }
+}
