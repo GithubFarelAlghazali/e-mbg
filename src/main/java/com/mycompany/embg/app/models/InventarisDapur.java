3513ffbbb7219b92aea83a7e42b6bf0785886830
GithubFarelAlghazali <farelgithub@gmail.com>
2026-06-03 14:53:35 +0700

    feat: add inventory and production class

---
+++ src/main/java/com/mycompany/embg/app/models/InventarisDapur.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.embg.app.models;
+import java.util.*;
+
+/**
+ *
+ * @author User
+ */
+public class InventarisDapur {
+    private ArrayList<BahanMakanan> bahanBahan;
+    
+    public void addBahan(BahanMakanan bahan){
+        bahanBahan.add(bahan);
+    }
+    
+    public void removeBahan(String nama){
+        
+    }
+    
+    public void tampilkanInventaris(){
+        
+    }
+    
+    public void cekStokKritis(){
+        
+    }
+}
